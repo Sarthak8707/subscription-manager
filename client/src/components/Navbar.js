ac8e4cc0fb0486193ba6002883228554da3a6cc8
sarthak <sarthakssingh49@gmail.com>
2025-12-22 01:46:24 +0530

    Changed Navbar and Homepage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"])
+    const navigate = useNavigate();
+    const signOut = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userID");
+        navigate("/auth");
+    }
   return (
     <div>
         <Link to="/">Home</Link>
         {"    "}
-        <Link to="/auth">Sign In</Link>
+        { cookies.access_token ?  <button onClick={signOut}>Sign Out</button> : <Link to="/auth">Sign In</Link>}
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useEffect } from 'react'
+import axios from "axios";
+import { useState } from 'react';
 
 const Home = () => {
+    const userID = window.localStorage.userID;
+    const [subs, setSubs] = useState([]);
+
+    useEffect(() => {
+        const fetchSubs = async () => {
+            const response = await axios.get(`http://localhost:3001/subscriptions/${userID}`);
+            console.log(response.data);
+            setSubs(response.data);
+        }
+        fetchSubs();
+    }, [])
+
   return (
-    <div>Home</div>
+    <div>
+        <h2>
+        Hello, User!
+        </h2>
+        <br/>
+        <h2>
+            Here are your Subscriptions:
+        </h2>
+        <br />
+        {subs.map((sub, idx) => (
+            <div>
+               {idx+1} {" "} {sub.name}
+            </div>
+        ))}
+
+         
+    </div>
   )
 }
 
